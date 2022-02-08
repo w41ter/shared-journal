b099e86ba064751ae3eb139045c4599d939d3a2d
w41ter <w41ter.l@gmail.com>
2022-02-08 17:48:41 +0800

    add Stream::seek, Stream::next (#53)

--- src/journal/stream.rs
+++ src/journal/stream.rs
@@ -12,32 +12,104 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use futures::channel::oneshot;
+use futures::{channel::oneshot, StreamExt, TryStreamExt};
 
 use super::worker::{Channel, Command};
-use crate::Result;
+use crate::{
+    master::{Master, RemoteMaster},
+    store::segment::{build_compound_segment_reader, CompoundSegmentReader},
+    Error, Result,
+};
 
 /// An increasing number to order events.
 pub type Sequence = u64;
 
-#[derive(Debug)]
-pub struct StreamReader {}
+#[allow(dead_code)]
+pub struct StreamReader {
+    stream_name: String,
+    current_epoch: u32,
+    start_index: Option<u32>,
+    segment_reader: Option<CompoundSegmentReader>,
+    master: RemoteMaster,
+}
+
+#[allow(dead_code)]
+impl StreamReader {
+    async fn switch_segment(&mut self) -> Result<()> {
+        let segment_meta = self
+            .master
+            .get_segment(&self.stream_name, self.current_epoch)
+            .await?;
+
+        let segment_meta = match segment_meta {
+            Some(meta) => meta,
+            None => {
+                return Err(Error::NotFound("no such stream".to_string()));
+            }
+        };
+
+        self.segment_reader = Some(
+            build_compound_segment_reader(
+                segment_meta.stream_id,
+                segment_meta.epoch,
+                segment_meta.copy_set,
+                std::mem::take(&mut self.start_index),
+            )
+            .await?,
+        );
+
+        Ok(())
+    }
+}
 
 #[allow(dead_code, unused)]
 impl StreamReader {
     /// Seeks to the given sequence.
     async fn seek(&mut self, sequence: Sequence) -> Result<()> {
-        todo!();
+        let epoch = (sequence >> 32) as u32;
+        let index = sequence as u32;
+        self.current_epoch = epoch;
+        self.start_index = Some(index);
+        self.switch_segment().await?;
+        Ok(())
     }
 
     /// Returns the next event.
     async fn try_next(&mut self) -> Result<Option<(Sequence, Vec<u8>)>> {
-        todo!();
+        match &mut self.segment_reader {
+            None => Err(Error::InvalidArgument("uninitialized".to_string())),
+            Some(reader) => {
+                // FIXME(w41ter) support try next;
+                let _value = reader.try_next().await?;
+                Ok(Some((0_u64, vec![0u8; 0])))
+            }
+        }
     }
 
     /// Returns the next event or waits until it is available.
-    async fn wait_next(&mut self) -> Result<(Sequence, Vec<u8>)> {
-        todo!();
+    async fn wait_next(&mut self) -> Result<Option<(Sequence, Box<[u8]>)>> {
+        loop {
+            match &mut self.segment_reader {
+                None => {
+                    return Err(Error::InvalidArgument("uninitialized".to_string()));
+                }
+                Some(reader) => {
+                    let value = match reader.next().await {
+                        Some(val) => val?,
+                        None => {
+                            self.current_epoch += 1;
+                            self.switch_segment().await?;
+                            continue;
+                        }
+                    };
+                    let value = (
+                        ((self.current_epoch as u64) << 32) | (value.0 as u64),
+                        value.1,
+                    );
+                    return Ok(Some(value));
+                }
+            }
+        }
     }
 }
 
