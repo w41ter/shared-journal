bbc068b5cdd66b55bf4f7c67794cd350c33404b6
w41ter <w41ter.l@gmail.com>
2022-02-17 16:42:28 +0800

    journal: progress add sliding window and congest state (#129)

--- src/journal/worker/progress.rs
+++ src/journal/worker/progress.rs
@@ -12,9 +12,83 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// An abstraction for a journal server to receive and persist entries.
-///
-/// For now, let's assume that message passing is reliable.
+use std::{collections::VecDeque, ops::Range};
+
+/// A mixin structure holds the fields used in congestion stage.
+#[allow(dead_code)]
+struct CongestMixin {
+    window_size: usize,
+
+    lost_bytes: usize,
+    recoup_bytes: usize,
+
+    latest_tick: usize,
+
+    retransmit_ranges: VecDeque<(Range<u32>, usize)>,
+}
+
+#[allow(dead_code)]
+struct SlidingWindow {
+    acked_bytes: usize,
+    send_bytes: usize,
+    capacity: usize,
+
+    start: usize,
+    size: usize,
+    window: Vec<(u32, usize)>,
+}
+
+impl SlidingWindow {
+    const KB: usize = 1024;
+    const MB: usize = 1024 * Self::KB;
+    const MIN_WINDOW: usize = 128;
+
+    fn new(capacity: usize) -> Self {
+        let window = Self::MIN_WINDOW.max(capacity / Self::MB);
+        SlidingWindow {
+            acked_bytes: 0,
+            send_bytes: 0,
+            capacity,
+
+            start: 0,
+            size: 0,
+            window: vec![(0, 0); window],
+        }
+    }
+
+    /// Returns the available space for the sliding window.
+    #[allow(dead_code)]
+    fn available(&self) -> usize {
+        if self.window.len() == self.size {
+            0
+        } else {
+            let consumed = self.send_bytes.saturating_sub(self.acked_bytes);
+            self.capacity.saturating_sub(consumed)
+        }
+    }
+
+    /// Allocate space and record relevant data.
+    fn replicate(&mut self, next_index: u32, bytes: usize) {
+        debug_assert!(self.size < self.window.len());
+
+        let off = (self.start + self.size) % self.window.len();
+        self.window[off] = (next_index, bytes);
+        self.size += 1;
+        self.send_bytes += bytes;
+    }
+
+    fn release(&mut self, received_index: u32) {
+        while self.size > 0 && self.window[self.start].0 <= received_index {
+            self.acked_bytes += self.window[self.start].1;
+            self.start = (self.start + 1) % self.window.len();
+            self.size -= 1;
+        }
+    }
+}
+
+/// An abstraction for describing the state of a segment store, that receives
+/// and persists entries.
+#[allow(dead_code)]
 pub(in crate::journal) struct Progress {
     epoch: u32,
 
@@ -25,6 +99,10 @@ pub(in crate::journal) struct Progress {
     start: usize,
     size: usize,
     in_flights: Vec<u32>,
+
+    sliding_window: SlidingWindow,
+
+    congest: Option<Box<CongestMixin>>,
 }
 
 impl Progress {
@@ -38,6 +116,9 @@ impl Progress {
             start: 0,
             size: 0,
             in_flights: vec![0; Self::WINDOW],
+
+            sliding_window: SlidingWindow::new(1024),
+            congest: None,
         }
     }
 
@@ -53,7 +134,6 @@ impl Progress {
     }
 
     pub fn replicate(&mut self, next_index: u32) {
-        debug_assert!(self.size < Self::WINDOW);
         debug_assert!(
             self.next_index <= next_index,
             "local next_index {}, but receive {}",
@@ -61,10 +141,7 @@ impl Progress {
             next_index
         );
         self.next_index = next_index;
-
-        let off = (self.start + self.size) % Self::WINDOW;
-        self.in_flights[off] = next_index;
-        self.size += 1;
+        self.sliding_window.replicate(next_index, 0);
     }
 
     /// A server has stored entries.
@@ -73,14 +150,21 @@ impl Progress {
             // Staled received request.
             false
         } else {
-            // TODO(w41ter) check epoch
-            while self.size > 0 && self.in_flights[self.start] < index {
-                self.start = (self.start + 1) % Self::WINDOW;
-                self.size -= 1;
-            }
+            self.sliding_window.release(index);
             self.matched_index = index;
             self.next_index = index + 1;
             self.size < Self::WINDOW
         }
+    }
+
+    #[allow(dead_code, unused)]
+    pub fn on_timeout(&mut self, range: std::ops::Range<u32>) -> bool {
+        false
+    }
+
+    /// Return whether a target has been matched to the corresponding index.
+    #[allow(dead_code, unused)]
+    pub fn is_matched(&self, index: u32) -> bool {
+        false
     }
 }
